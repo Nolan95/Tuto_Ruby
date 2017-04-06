71d13b109087050241bf56cde3a1c6337af295ca
sadate <sadatetchamouza@gmail.com>
2017-04-06 11:46:54 +0200

    Petite modification!!

--- exo_compteur.rb
+++ exo_compteur.rb
@@ -1,6 +1,6 @@
 test = "I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. I can't get involved! I've got work to do! It's not that I like the Empire, I hate it, but there's nothing I can do about it right now. It's such a long way from here"
 frequence = Hash.new(0)
-mots = test.downcase.tr('.",', ' ').split(' ')
+mots = test.downcase.tr('.",:', ' ').split(' ')
 mots.each do |mot|
   frequence[mot] += 1
 end
